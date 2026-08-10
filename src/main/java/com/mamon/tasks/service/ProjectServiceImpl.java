0fc9d993ff1c91786b2d2e327d4819b7f603d60b
Mamoun44 <mkhatmtm@gmail.com>
2026-08-10 16:27:18 +0300

    Version 1 - working project

--- src/main/java/com/mamon/tasks/service/ProjectServiceImpl.java
+++ src/main/java/com/mamon/tasks/service/ProjectServiceImpl.java
@@ -1,8 +1,5 @@
 package com.mamon.tasks.service;
 
-import com.mamon.tasks.dto.ProjectResponseDto;
-import com.mamon.tasks.exception.ProjectNotFoundException;
-import com.mamon.tasks.mapper.ProjectMapper;
 import com.mamon.tasks.model.Project;
 import com.mamon.tasks.model.Task;
 import com.mamon.tasks.repository.ProjectRepository;
@@ -11,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
@@ -25,17 +21,13 @@ public class ProjectServiceImpl implements ProjectService {
     }
 
     @Override
-    public List<ProjectResponseDto> getAllProjects() {
-        return projectRepository.findAll().stream()
-                .map(ProjectMapper::toResponseDto)
-                .collect(Collectors.toList()).reversed();
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
     }
 
     @Override
-    public ProjectResponseDto getProjectById(Long id) {
-        Project project = projectRepository.findById(id)
-                .orElseThrow(() -> new ProjectNotFoundException(id));
-        return ProjectMapper.toResponseDto(project);
+    public Project getProjectById(Long id) {
+        return projectRepository.findById(id).orElse(null);
     }
 
     @Override
